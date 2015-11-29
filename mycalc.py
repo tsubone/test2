da25e4349433b08cde5d3dfcd1e9c00121c98eb1
Takashi Tsubone <tsubone@ubuntu>
2015-11-29 08:57:53 -0800

    update.

--- mycalc.py
+++ mycalc.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+
 # my calc.py
 # for test
 
 def add(a, b):
     return a+b
+
+print "__package__ ==", __package__
+print "__file__ ==", __file__
+print "__name__ ==", __name__
