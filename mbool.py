0ba9c806d3b2b2fc50f9b42b88e4a350d1c3ba7f
Takashi Tsubone <tsubone@ubuntu>
2015-11-29 07:27:23 -0800

    update.

--- mbool.py
+++ mbool.py
@@ -2,13 +2,49 @@
 
 import enum
 
-Mbool = enum.Enum("Mbool", "T F I M")
+Mbool = enum.Enum("Mbool", "T I F M")
 
 def IsMbool(a):
     if a == Mbool.T or a == Mbool.F or a == Mbool.I or a == Mbool.M:
         return Mbool.T
     else:
         return Mbool.F
+
+MandTbl=((Mbool.T, Mbool.I, Mbool.F, Mbool.M),
+         (Mbool.I, Mbool.I, Mbool.F, Mbool.M),
+         (Mbool.F, Mbool.F, Mbool.F, Mbool.M),
+         (Mbool.M, Mbool.M, Mbool.M, Mbool.M))
+
+MorTbl=((Mbool.T, Mbool.T, Mbool.T, Mbool.T),
+        (Mbool.T, Mbool.I, Mbool.I, Mbool.I),
+        (Mbool.T, Mbool.I, Mbool.F, Mbool.F),
+        (Mbool.T, Mbool.I, Mbool.F, Mbool.M))
+
+MnotTbl=(Mbool.F, Mbool.M, Mbool.T, Mbool.I)
+
+def Mand(a,b):
+    if IsMbool (a) != Mbool.T:
+        return Mbool.M
+    elif IsMbool (b) != Mbool.T:
+        return Mbool.M
+
+    return MandTbl[a.value-1][b.value-1]
+
+def Mor(a,b):
+    if IsMbool (a) != Mbool.T:
+        return Mbool.M
+    elif IsMbool (b) != Mbool.T:
+        return Mbool.M
+
+    return MorTbl[a.value-1][b.value-1]
+
+
+def Mnot(a):
+    if IsMbool (a) != Mbool.T:
+        return Mbool.M
+
+    return MnotTbl[a.value-1];
+
 
 print Mbool(1)
 print Mbool(2)
@@ -26,11 +62,25 @@ else:
 kkk = "hoo:" + IsMbool(Mbool.M).name
 print kkk
 
+print "kkk=%s,%i" % (kkk, 1)
+
 for i in Mbool:
     print i
 
-def mand(a,b):
-    print ""
+print MandTbl
 
-print "Hello world\n"
+print "Hello"
+
+print Mand (1, 2)
+print Mand (Mbool.T, Mbool.F)
+print a.value
+
+print "=====result======="
+
+for i in Mbool:
+    for j in Mbool:
+        a = Mnot(Mor(i, j))
+        b = Mand(Mnot(i), Mnot(j))
+        print i, j, a == b
+
 
