f4d62f096685945856401e86a1235ceeefef2a8f
Takashi Tsubone <takashi@ubuntu>
2015-12-04 22:33:02 -0800

    update.

---
+++ qml_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtDeclarative import QDeclarativeView
+
+app = QApplication(sys.argv)
+view = QDeclarativeView ()
+url = QUrl('view.qml')
+view.setSource(url)
+view.show()
+
+sys.exit(app.exec_())
+
