2680d75bcabeb06940a842e550968d59e2fb05dc
Takashi Tsubone <takashi@ubuntu>
2015-12-06 04:21:32 -0800

    update.

--- url/url_test.py
+++ url/url_test.py
@@ -3,6 +3,17 @@
 import urllib
 
 response = urllib.urlopen ("http://www.asahi.com")
-print 'RESPONSE' :, response
-print 'URL'      :, response.geturl()
+print 'RESPONSE:', response
+print 'URL     :', response.geturl()
 
+headers = response.info ()
+print 'DATE    :', headers['date']
+print 'HEADERS :'
+print '----------'
+print headers
+
+data = response.read ()
+print 'LENGTH  :', len(data)
+print 'DATA    :'
+print '----------'
+print data
