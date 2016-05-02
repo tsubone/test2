453a986a2ecd188c0b5d0304c72b15b1fb1e95c1
Takashi Tsubone <takashi@newton>
2016-05-02 13:30:09 +0900

    add.

---
+++ chainer/relu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+#from sklearn.datasets import fetch_mldata
+from chainer import cuda, Variable, FunctionSet, optimizers
+import chainer.functions  as F
+import sys
+
+plt.style.use('ggplot')
+
+
+# F.reluテスト
+x_data = np.linspace(-10, 10, 100, dtype=np.float32)
+x = Variable(x_data)
+y = F.relu(x)
+
+plt.figure(figsize=(7,5))
+plt.ylim(-2,10)
+plt.plot(x.data, y.data)
+plt.show()
